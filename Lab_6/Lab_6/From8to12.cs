1d4fb128e8d34edc165591f6acb05a6d1d891d5f
Katysheva <polly995@yandex.ru>
2014-10-16 14:24:04 +0400

    created new project for lab_6.
    created test project with one test

---
+++ Lab_6/Lab_6/From8to12.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    public class OctTo12
+    {
+        public static object OctToInt(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lab_6/UnitTestProject_Lab_6/From8to12_Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab_6;
+
+namespace UnitTestProject_Lab_6
+{
+    [TestClass]
+    public class From8to12_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var number = 32;
+            var expected = 26;
+            var actual = OctTo12.OctToInt(number);
+            Assert.AreEqual(expected, actual);
+            
+        }
+    }
+}
