32611b1e2264ef0e4493e8df1e562f46de7fcb23
Katysheva <polly995@yandex.ru>
2014-12-16 12:23:40 +0300

    test project created

---
+++ ScrabbleWinForm/ScrabbleTestProject/ScrabbleTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScrabbleWinForm;
+
+namespace ScrabbleTestProject
+{
+    [TestClass]
+    public class ScrabbleTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
